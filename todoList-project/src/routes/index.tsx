ca4d9a57a6ef1669567405b6c6c2a9988201323b
Lucas <lucas_emanuel_sc@hotmail.com>
2023-06-28 14:40:28 -0300

    chore: settings

---
+++ todoList-project/src/routes/index.tsx
@@ -0,0 +1,12 @@
+import { createNativeStackNavigator} from '@react-navigation/native-stack'
+import Login from '../pages/Login';
+
+const Stack = createNativeStackNavigator();
+
+export default function Routes() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
+    </Stack.Navigator>
+  )
+};
